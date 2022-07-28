dfe0e570f809fbde2893c7deb66aadd541d27778
Wellington Ideão <wellington.ideao@gmail.com>
2022-07-28 13:25:17 -0300

    Diretiva estrutural ngIf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'template';
+  public exibirConteudo = false;
+
+  public toggleConteudo(){
+    this.exibirConteudo = !this.exibirConteudo;
+  }
 }
